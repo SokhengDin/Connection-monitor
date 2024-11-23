642b7ecafe85d6f0e46bb7b56197f69fc111c868
SokhengDin <din.sokheng08@gmail.com>
2024-11-23 12:19:38 +0700

    asdf

--- src/server/publisher.ts
+++ src/server/publisher.ts
@@ -143,7 +143,7 @@ const gracefulShutdown = async () => {
 };
 
 const port = parseInt(process.env.PORT || '3000', 10);
-app.listen(port, '0.0.0.0', () => {
+httpServer.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
 });
 
